0ae9f383afb68da3a5db072e64c65afc145cad20
Audreal <yjhoj@outlook.com>
2026-01-20 14:43:25 +0100

    Toggle sensor state with button
    Updated IR Remote code to include button functionality.

--- src/modules/IR_Remote/IR_Remote.ino
+++ src/modules/IR_Remote/IR_Remote.ino
@@ -1,17 +1,26 @@
 #include <IRremote.hpp>
 
-const uint8_t inPin = 11;
-void setup()
-{
+const uint32_t buttonOne = 0xF30CFF00;
+const uint8_t inPin = 8;
+bool systemState = HIGH;
+
+void setup(){
     Serial.begin(115200);
     IrReceiver.begin(inPin, ENABLE_LED_FEEDBACK);
 }
 
-void loop()
-{
-    if (IrReceiver.decode())
-    {
-        Serial.println(IrReceiver.decodedIRData.decodedRawData, HEX);
-        IrReceiver.resume();
+void loop(){
+  if (IrReceiver.decode()) {
+    uint32_t receivedCode = IrReceiver.decodedIRData.decodedRawData;
+    if (receivedCode == buttonOne && receivedCode != 0) {
+      systemState = !systemState;
+      if (systemState == 0) {
+        Serial.println("Sensor OFF!");
+      }
+      if (systemState == 1) {
+        Serial.println("Sensor ON!");
+      }
     }
+    IrReceiver.resume();
+  }
 }
