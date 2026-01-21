2b81cf444d2a0ef2223fe23be3dc964d631947aa
Audreal <yjhoj@outlook.com>
2026-01-21 12:25:25 +0100

    Added doxygen comments

--- src/modules/Sensor1/Sensor1.ino
+++ src/modules/Sensor1/Sensor1.ino
@@ -1,10 +1,26 @@
+/**
+ * @file Sensor1.ino
+ * @author Yun Jie Si Høj (s224179@dtu.dk)
+ * @brief Flame detector sensor module code
+ * @version 0.1
+ * @date 2026-01-21
+ * 
+ * @copyright Copyright (c) 2026
+ * 
+ */
 #include "sender_comm.h"
 
+/**
+ * @brief Pin definitions and global variables. Each module has its own unique sensorID.
+ */
 const uint8_t flamePin1 = A0;
 const uint8_t ledPin = D2;
 const uint8_t sensorID = 4;
 uint8_t fireCounter = 0;
-
+/**
+ * @brief Setup function to initialize serial communication, pin modes, and WiFi
+ * @author Yun Jie Si Høj
+ */
 void setup() {
   Serial.begin(115200);
   pinMode(flamePin1, INPUT);
@@ -12,17 +28,29 @@ void setup() {
   initWifi();
 }
 
+/**
+ * @brief Main loop function to read sensor data and handle fire detection
+ * @author Yun Jie Si Høj
+ */
 void loop() {
   sensor1();
   delay(300);
 }
 
+/**
+ * @brief Function to blink an LED for visual indication
+ * @author Yun Jie Si Høj
+ */
 void blink() {
   digitalWrite(ledPin, HIGH);
   delay(300);
   digitalWrite(ledPin, LOW);
 }
-
+/**
+ * @brief Function to read flame sensor data and send alert to the HUB if fire is detected.
+ * All the json handling is done in sender_comm.h.
+ * @author Yun Jie Si Høj
+ */
 void sensor1() {
   float flameReading = analogRead(flamePin1);
   Serial.println(flameReading);
@@ -43,6 +71,9 @@ void sensor1() {
       jsonSend();
       Serial.print("Warn HUB!");
     }
+  /**
+   * @brief Else reset the fireCounter.
+   */
   } else {
     fireCounter = 0;
   }
